fbd739a4b879c9e7b9e7d5ee565af162b60368ca
Alfred Persson Forsberg <cat@catcream.org>
2024-04-05 00:51:59 +0200

    implement copyvec preparation
    Signed-off-by: Alfred Persson Forsberg <cat@catcream.org>

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <stdint.h>
-void copyvec(int32_t dest[], int16_t src[], int32_t size);
+void copyvec(int16_t dest[], int32_t src[], int32_t size);
 
 int main(void) {
-  int32_t dest[4] = {0};
-  int16_t src[4] = {1,2,3,4};
+  int16_t dest[4] = {0};
+  int32_t src[4] = {1,2,3,4};
   copyvec(dest, src, 4);
   return 0;
 }
