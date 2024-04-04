32b9ec005b40a8ed6ba507bd6e6f0c7387e43e58
Alfred Persson Forsberg <cat@catcream.org>
2024-04-04 21:04:41 +0200

    implement copyvec
    Signed-off-by: Alfred Persson Forsberg <cat@catcream.org>

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
-void copyvec(int dest[], short src[], int size);
+#include <stdint.h>
+void copyvec(int32_t dest[], int16_t src[], int32_t size);
 
 int main(void) {
-    copyvec(nullptr, nullptr, 0);
-    return 0;
+  int32_t dest[4] = {0};
+  int16_t src[4] = {1,2,3,4};
+  copyvec(dest, src, 4);
+  return 0;
 }
