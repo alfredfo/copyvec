b25a04715247c108fad9c336d47257389fde5267
Alfred Persson Forsberg <cat@catcream.org>
2024-04-05 01:17:15 +0200

    fixup! add copyelements target
    Signed-off-by: Alfred Persson Forsberg <cat@catcream.org>

--- src/main_copyelements.c
+++ src/main_copyelements.c
@@ -3,7 +3,7 @@ void copyelements(int32_t src[], int16_t dst[], int32_t start, int32_t end);
 
 int main(void) {
   int16_t dst[8] = {0};
-  int32_t src[8] = {1,2,3,4, -1, -2, -3, -4};
-  copyelements(src, dst, 2, 6);
+  int32_t src[8] = {1, 2, 3, 4, -1, -2, -3, -4};
+  copyelements(src, dst,  2, 6);
   return 0;
 }
--- src/main_copyvec.c
+++ src/main_copyvec.c
@@ -2,8 +2,8 @@
 void copyvec(int16_t dest[], int32_t src[], int32_t size);
 
 int main(void) {
-  int16_t dest[4] = {0};
-  int32_t src[4] = {1,2,3,4};
+  int16_t dest[4] = { 0 };
+  int32_t src[4] = {1, 2, 3, 4};
   copyvec(dest, src, 4);
   return 0;
 }
